020c8e42cd4577ef246a0d136d1b118766b2d755
praveen12191 <haripraveen816@gmail.com>
2024-03-02 13:25:33 +0530

    pdfUpdate

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+import { Document, Page } from 'react-pdf';
+import PdfViewer from './components/PdfViewer';
+
+const App = () => {
+	return (
+		<PdfViewer></PdfViewer>
+	);
+};
+
+export default App;
---
+++ src/components/PdfViewer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+
+import pdf from './lung.pdf'
+ 
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.js`;
+
+const PdfViewer = () => {
+
+	const [numPages, setNumPages] = useState(null);
+	const [pageNumber, setPageNumber] = useState(1);
+
+	const onDocumentLoadSuccess = ({ numPages }) => {
+		setNumPages(numPages);
+	};
+
+	const goToPrevPage = () =>
+		setPageNumber(pageNumber - 1 <= 1 ? 1 : pageNumber - 1);
+
+	const goToNextPage = () =>
+		setPageNumber(
+			pageNumber + 1 >= numPages ? numPages : pageNumber + 1,
+		);
+
+	return (
+		<div>
+			<nav>
+				<button onClick={goToPrevPage}>Prev</button>
+				<button onClick={goToNextPage}>Next</button>
+				<p>
+					Page {pageNumber} of {numPages}
+				</p>
+			</nav>
+
+			<Document
+				file={pdf}
+				onLoadSuccess={onDocumentLoadSuccess}
+			>
+				<Page pageNumber={pageNumber} />
+			</Document>
+		</div>
+	);
+};
+
+export default PdfViewer;
